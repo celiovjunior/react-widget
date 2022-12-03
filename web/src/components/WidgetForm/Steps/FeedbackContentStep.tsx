d6b90287737020c972fc6d74c959e830dc1ee604
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2022-12-03 11:01:59 -0300

    made the navigation in the widget feedback steps

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,31 @@
-export function FeedbackContentStep() {
+import { ArrowLeft } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from ".."
+import { CloseButton } from "../../CloseButton"
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackTypeRestartRequested: () => void;
+}
+
+export function FeedbackContentStep({ feedbackType, onFeedbackTypeRestartRequested }: FeedbackContentStepProps) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType]
+
   return(
+    <>
+    <header>
+      <button onClick={onFeedbackTypeRestartRequested} type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100">
+        <ArrowLeft weight="bold" className="h-4 w-4" />
+      </button>
+      <span className="text-xl leading-6 flex items-center gap-2">
+        <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+        {feedbackTypeInfo.title}
+      </span>
+      <CloseButton />
+    </header>
     
+    <div className="flex py-8 gap-2 w-full">
+
+    </div>
+  </>
   )
 }
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -4,6 +4,7 @@ import ideaImageUrl from '../../assets/idea.svg'
 import thoughtImageUrl from '../../assets/thought.svg'
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -34,16 +35,22 @@ export type FeedbackType = keyof typeof feedbackTypes
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+  function handleRestartFeedback() {
+    setFeedbackType(null)
+  }
+
   return(
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header className="">
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
 
-      {!feedbackType ? <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />  : (
-        <p>Hello world!</p>
-      )}
+
+      {!feedbackType ? 
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />  
+        : 
+        <FeedbackContentStep
+         onFeedbackTypeRestartRequested={handleRestartFeedback}
+         feedbackType={feedbackType} 
+        />
+      }
       
       <footer>
         Feito com ❤ por <a className="underline underline-offset-2" href="https://github.com/celiovjunior">Célio Jr.</a>
