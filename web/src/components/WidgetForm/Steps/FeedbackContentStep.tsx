a3cc4186cc00a4af65146884b3a6330ee4568ce6
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2022-12-03 11:40:02 -0300

    finish the styles to the form

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -13,7 +13,11 @@ export function FeedbackContentStep({ feedbackType, onFeedbackTypeRestartRequest
   return(
     <>
     <header>
-      <button onClick={onFeedbackTypeRestartRequested} type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100">
+      <button 
+       onClick={onFeedbackTypeRestartRequested} 
+       type="button" 
+       className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+      >
         <ArrowLeft weight="bold" className="h-4 w-4" />
       </button>
       <span className="text-xl leading-6 flex items-center gap-2">
@@ -23,9 +27,25 @@ export function FeedbackContentStep({ feedbackType, onFeedbackTypeRestartRequest
       <CloseButton />
     </header>
     
-    <div className="flex py-8 gap-2 w-full">
-
-    </div>
+    <form className="my-4 w-full">
+      <textarea
+        className="
+        min-w-[304px] 
+        w-full min-h-[112px] 
+        text-sm 
+        placeholder-zinc-400 
+        text-zinc-100 
+        border-zinc-600 
+        bg-transparent 
+        rounded-md 
+        focus:border-brand-500
+        focus:ring-brand-500
+        focus:ring-1
+        resize-none
+        focus:outline-none"
+        placeholder="Conte com detalhes o que está acontecendo..."
+      />
+    </form>
   </>
   )
 }
