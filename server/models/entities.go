938a94463532bfb4b02ba2bbf85fd7a3cbb5f94e
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2022-12-03 20:05:56 -0300

    opened the connection with database and start to made the first http request using SQL query

---
+++ server/models/entities.go
@@ -0,0 +1,8 @@
+package models
+
+type Feedback struct {
+	ID         int64  `json: "id"`
+	Type       string `json: "type"`
+	Comment    string `json: "comment"`
+	Screenshot string `json: "screenshot"`
+}
